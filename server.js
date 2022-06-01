c5f1756145cd5a41d7409920bd51d1ce1b428392
githun DOHAN25 <joke3323@naver.com>
2022-06-01 21:21:51 +0900

    install multer

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const models = require("./models");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const port = 8080;
 
 app.use(express.json());
